0549bea12365f7fd06db9faf2c0f008d349c474a
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-10-20 09:33:12 +0530

    Initial Commit

---
+++ BankingUI/BankingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingUIApp.swift
+//  BankingUI
+//
+//  Created by Aayush kumar on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BankingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankingUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BankingUI
+//
+//  Created by Aayush kumar on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
